54fb3b62955e36f4972be9602299f413967d5489
mmarelli1985 <marco.marelli.85@gmail.com>
2016-07-12 14:26:38 +0200

    ciao

---
+++ dashboard/js/anagrafiche.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	var jsonArray = {
+		"Result": "OK",
+		"Records": [
+			{ "Id": 123456,
+			"Nome": "DV",
+			"Cognome": 18,
+			"CF": 55,
+			"DataNascita": "Scadenza Garcia",
+			"CittaNascita": "agatha.garcia@jtable.org",
+			"ProvNasciita": "123", "Stato": "F"},
+			"CAPNasciita": "123", "Stato": "F"},
+
+			"CittaResidenza": "agatha.garcia@jtable.org",
+			"ProvResidenza": "123", "Stato": "F"},
+			"CAPResidenza": "123", "Stato": "F"},
+			"ViaResidenza": "123", "Stato": "F"},
+
+			
+			],
+		"TotalRecordCount": 1};	
+	
+});
--- prov/js/dashboard.js
+++ prov/js/dashboard.js
@@ -1,10 +1,4 @@
-    $(document).ready(function () {
-	
-		$("#carousel").carousel({
-			pause: true,
-			interval: false
-		});
-	
+    $(document).ready(function () {	
 		var jsonArray = {
 					"Result": "OK",
 					"Records": [
@@ -33,50 +27,6 @@
 				
 				
 			});
-
-			$("#ball-filter-reset").on("click",function(){
-				jsonArray = {
-					"Result": "OK",
-					"Records": [
-						{ "IdPratica": 123456, "TipoCertificato": "DV", "Cliente": 18, "Dominio": 55, "Attivazione": "Scadenza Garcia", "Scadenza": "agatha.garcia@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123457, "TipoCertificato": "DV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123458, "TipoCertificato": "DV", "Cliente": 18, "Dominio": 55, "Attivazione": "Scadenza Garcia", "Scadenza": "agatha.garcia@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123459, "TipoCertificato": "OV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123460, "TipoCertificato": "OV", "Cliente": 18, "Dominio": 55, "Attivazione": "Scadenza Garcia", "Scadenza": "agatha.garcia@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123461, "TipoCertificato": "OV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},						
-						{ "IdPratica": 123462, "TipoCertificato": "EV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123463, "TipoCertificato": "EV", "Cliente": 18, "Dominio": 55, "Attivazione": "Scadenza Garcia", "Scadenza": "agatha.garcia@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123464, "TipoCertificato": "EV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},						
-						],
-						"TotalRecordCount": 9};				
-			$('#studentTableContainer').jtable('load');
-			});
-
-			$("#cert-ov-ball").on("click",function(){
-				jsonArray = {
-					"Result": "OK",
-					"Records": [
-						{ "IdPratica": 123459, "TipoCertificato": "OV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123460, "TipoCertificato": "OV", "Cliente": 18, "Dominio": 55, "Attivazione": "Scadenza Garcia", "Scadenza": "agatha.garcia@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123461, "TipoCertificato": "OV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},						
-						],
-						"TotalRecordCount": 3};				
-			$('#studentTableContainer').jtable('load');
-			});
-			
-			$("#cert-ev-ball").on("click",function(){
-				jsonArray = {
-					"Result": "OK",
-					"Records": [
-						{ "IdPratica": 123462, "TipoCertificato": "EV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123463, "TipoCertificato": "EV", "Cliente": 18, "Dominio": 55, "Attivazione": "Scadenza Garcia", "Scadenza": "agatha.garcia@jtable.org", "Durata": "123", "Stato": "F"},
-						{ "IdPratica": 123464, "TipoCertificato": "EV", "Cliente": 1, "Dominio": 1, "Attivazione": "Scadenza Lafore", "Scadenza": "agatha.lafore@jtable.org", "Durata": "123", "Stato": "F"},						
-						],
-						"TotalRecordCount": 3};				
-			$('#studentTableContainer').jtable('load');
-				
-				
-			});			
 			
 			$("#studentTableContainer").jtable({
 				paging: true, //Enable paging
